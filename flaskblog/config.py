9fb2396dcacb41a3abe1b3168c6d6f5c94c1513f
Mark Victorio <mvictorio79@gmail.com>
2021-08-14 17:39:53 +0800

    Commit Start

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = 'db89dd76348c916b4f6d3d2bebaf3648'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'CSStudy2G2@gmail.com'
+    MAIL_PASSWORD = 'Am0Gu$u$'
