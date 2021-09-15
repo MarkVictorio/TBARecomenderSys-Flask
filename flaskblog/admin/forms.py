3457b2606cac845fdb62885608e8ce82354ca54a
Mark Victorio <mvictorio79@gmail.com>
2021-09-16 05:38:44 +0800

    USER_INPUT NOW PASSING WAHAHAHA

--- flaskblog/admin/forms.py
+++ flaskblog/admin/forms.py
@@ -1,19 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, FieldList, FormField, Form
-from wtforms.fields.core import IntegerField
+from wtforms import StringField, SubmitField, TextAreaField, FieldList, FormField, RadioField
+from wtforms.fields.core import BooleanField, IntegerField
 from wtforms.validators import DataRequired
 
-
-class QuestionForm(Form):
-    question = StringField('Question',validators=[DataRequired()])
-    choice_1 = StringField('Choice 1',validators=[DataRequired()])
-    choice_2 = StringField('Choice 2',validators=[DataRequired()])
-    choice_3 = StringField('Choice 3',validators=[DataRequired()])
-    choice_4 = StringField('Choice 4',validators=[DataRequired()])
-    answer = IntegerField('Answer',validators=[DataRequired()])
-
-class QuizForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    description = TextAreaField('Quiz Desciption', validators=[DataRequired()])
-    items = FieldList(FormField(QuestionForm),min_entries=2)
-    submit = SubmitField('Create')
+class UserInputForm(FlaskForm):
+    radio = RadioField(label='label',choices=[])
+    user_answer = IntegerField('Answer',validators=[DataRequired()])
+    is_correct = BooleanField('Correct', validators=[DataRequired()])
--- flaskblog/admin/routes.py
+++ flaskblog/admin/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask import (render_template, url_for, flash,
                    redirect, request, abort, Blueprint)
 from flask_login import current_user, login_required
-from flaskblog.admin.forms import (QuestionForm, QuizForm)
+from flaskblog.admin.forms import UserInputForm
 from flaskblog.models import Quiz, Question
 from flaskblog import db
 
@@ -31,31 +31,14 @@ def quiz(quiz_id):
     quizzes = Quiz.query.get_or_404(quiz_id)
     page = request.args.get('page', 1, type=int)
     questions = Question.query.filter_by(quiz_id=quiz_id).order_by(Question.quiz_id.asc()).paginate(page=page, per_page=20)
-    u_choice = request.form.get('choices')
-    return render_template('quiz.html', title = quizzes.title ,     
-    u_choice = u_choice, quizzes = quizzes, questions = questions)
-
-@admin.route("/create_quiz" , methods=['GET', 'POST'])
-@login_required
-def create_quiz():
-    if current_user.admin != 1:
-        return redirect(url_for('main.home'))
-    
-    form = QuizForm()
-    if form.validate_on_submit():
-        quiz = Quiz(title = form.title.data, description = form.description.data, total_score = 10) 
-        db.session.add(quiz)
-        
-        for field in form.items():
-            question = Question(item_desc = field.question.data,answer = field.answer.data,     
-            choice_1 = field.choice_1.data, choice_2 = field.choice_2.data,    
-            choice_3 = field.choice_3.data, choice_4 = field.choice_4.data)
-            db.session.add(question)
-        
-        db.session.commit()
-        flash('Your quiz has been created!', 'success')
-        return redirect(url_for('main.home'))
-    
-    return render_template('create_quiz.html', title='New Quiz',
-                           form=form, legend='New Quiz')
-
+    total = Question.query.filter_by(quiz_id=quiz_id).count()
+    form = UserInputForm()
+    u_choice = request.form.get("Choice ")
+    user_choice = []
+   
+    for i in range(total):
+        index = str(i + 1)
+        u_choice = request.form.get("Choice "+index)
+        user_choice.append(u_choice)
+    return render_template('quiz.html', title = quizzes.title,     
+        quizzes = quizzes, questions = questions, total = total, form = form, user_choice=user_choice)
